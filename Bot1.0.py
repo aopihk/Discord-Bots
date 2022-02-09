c750717670006f96e46eba3468421b6792292284
aopihk <alek.neely13@gmail.com>
2022-02-09 14:21:15 -0500

    Added functionality to support requesting up-to-date weather information on any requested city.

--- Bot1.0.py
+++ Bot1.0.py
@@ -1,9 +1,14 @@
 import discord
+import requests, json
 from datetime import datetime, timedelta
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+degreeSign = u"\N{DEGREE SIGN}"
+OpenWeatherKey = "30d8c43f0d2614036e2957739678963a"
 
 #discord configuration w/ API
 #this requires a valid discord token that is provided when you register the bot with Discord
-TOKEN = ''
+TOKEN = 'OTQxMDM2NzM1ODY4MDQzMzE1.YgQGwQ.Yb-_W8DSw533XjwoWflrF8fs6Nw'
 SERVER = 'BotTestingGround'
 
 client = discord.Client()
@@ -49,9 +54,36 @@ async def on_message(message):
     #dont trigger on bot-sent messages
     if message.author == client.user:
         return
+
     #dont trigger in defined channels
-    if (message.channel.name == 'bot-feature-requests') or (message.channel.name == 'dj-booth'):
+    if (message.channel.name == 'demo-channel'): #or (message.channel.name == 'test-channel'):
         return
+
+    #!weather CITYNAME triggers the bot to print a basic weather report of the inputted city
+    if ('!weather' in message.content.lower()):
+
+        #get city name from second part of argument string and build URL request w/ OpenWeatherMap developer API key
+        CITY = str(message.content)[9:]
+        URL = BASE_URL + "q=" + CITY + "&units=imperial" + "&appid=" + OpenWeatherKey
+
+        response = requests.get(URL)
+        if response.status_code == 200:
+
+            data = response.json()
+
+            #store weather information from json payload
+            primaryWeatherDataDict = data['main']
+            temperature = primaryWeatherDataDict['temp']
+            humidity = primaryWeatherDataDict['humidity']
+            weatherDescriptionDict = data['weather']
+            cityName = data['name']
+
+            #build formatted string to print information in the discord text channel
+            stringToPrint = "Weather Report for: " + cityName + "\n The current temperature is " + str(temperature) + degreeSign + "F" + "\n The current humidity is " + str(humidity) + "%" + "\n Other information: " + weatherDescriptionDict[0]['description']
+            await message.channel.send(stringToPrint)
+
+        else:
+            print("ERROR - API KEY LIKELY BROKEN")
 
     #!changenickname / targetuser / targetnickname
     #this code allows a user to change the nickname of another user (if the bot has permission configured on the server)
@@ -70,7 +102,7 @@ async def on_message(message):
     #most of the code is implementing a basic "timeout" - if the bot sends 5 messages within 5 minutes
     #it will stop responding until another 5 minutes has passed
 
-    if 'dead cord' in message.content.lower():
+    if 'hello bot' in message.content.lower():
         currentTime = datetime.now()
         if lastCooldownDate + timedelta(minutes=5) >= currentTime:
             return
@@ -83,7 +115,7 @@ async def on_message(message):
         if msgCount == 0:
             firstMsgDate = datetime.now()
         msgCount = msgCount + 1
-        await message.channel.send('ironic')
+        await message.channel.send('hi alek')
 
 
 client.run(TOKEN)
